9de198056f877b76c5b9e73c36a565d8f886acc0
NPontious <nicholas.pontious@gmail.com>
2026-04-06 13:04:41 -0400

    desolo key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,6 +4,7 @@ let
   glacio_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDASAHScag9TBoXy0oZKTR67BykcH5g5HTrqLsbABLnl root@glacio";
   vesania_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQmu30vMqi1V8v8iUk3/EF6r8+l+ujSruqq+At7j3oj root@vesania";
   sylva_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBn2c/9xEqfTocaT2HfIAjEKYgJytv/dUq5Kr9TDdhCv root@sylva";
+  desolo_host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINhXX59EL8xxxnMQkaPYD+J1s70JXiAPFqBlPSNsFYDJ root@nixos";
   
   admins = [ nicho_vesania glacio_host vesania_host sylva_host ];
   systems = [ glacio_host vesania_host sylva_host ];
