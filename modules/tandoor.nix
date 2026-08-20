38783242d3f80dc5cebacb6fc63caa338041387c
NPontious <nicholas.pontious@gmail.com>
2026-08-19 20:08:19 -0400

    tandoor fix

--- modules/tandoor.nix
+++ modules/tandoor.nix
@@ -8,15 +8,19 @@
       enable = true;
       port = 8082;
       address = "0.0.0.0";
-      djangoSecretKeyFile = config.age.secrets."webui-secret-key".path;
       extraConfig = {
+        HOME = "/var/lib/tandoor-recipes";
         MEDIA_ROOT = "/mnt/storage/Pictures/tandoor";
+        SECRET_KEY_FILE = config.age.secrets."webui-secret-key".path;
+        ALLOWED_HOSTS = "*";
+        CSRF_TRUSTED_ORIGINS = "http://glacio:8082,http://100.85.234.127:8082,http://glacio.tail4a2d4d.ts.net:8082";
       };
     };
 
     age.secrets."webui-secret-key" = {
       file = ../secrets/webui-secret-key.age;
       mode = "0400";
+      owner = "tandoor_recipes";
     };
   };
 }
